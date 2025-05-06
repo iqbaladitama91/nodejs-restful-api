88bdd22b52fbc02d8b03dd43034181c4ca1ef382
Iqbal Aditama <iqbaladitama91@gmail.com>
2025-05-06 21:17:51 +0700

    feat: update

---
+++ test/user.test.js
@@ -0,0 +1,65 @@
+import supertest from "supertest";
+import { web } from "../src/application/web.js";
+import { prismaClient } from "../src/application/database.js";
+import { logger } from "../src/application/logging.js";
+
+describe("POST /api/users", function () {
+  afterEach(async () => {
+    await prismaClient.user.deleteMany({
+      where: {
+        username: "iqbaladitama",
+      },
+    });
+  });
+  it("should can register new user", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "iqbaladitama",
+      password: "rahasia",
+      name: "Muhammad Iqbal Aditama",
+    });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe("iqbaladitama");
+    expect(result.body.data.name).toBe("Muhammad Iqbal Aditama");
+    expect(result.body.data.password).toBeUndefined();
+  });
+
+  it("should reject if request is invalid", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("should reject if user already registered", async () => {
+    let result = await supertest(web).post("/api/users").send({
+      username: "iqbaladitama",
+      password: "rahasia",
+      name: "Muhammad Iqbal Aditama",
+    });
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe("iqbaladitama");
+    expect(result.body.data.name).toBe("Muhammad Iqbal Aditama");
+    expect(result.body.data.password).toBeUndefined();
+
+    result = await supertest(web).post("/api/users").send({
+      username: "iqbaladitama",
+      password: "rahasia",
+      name: "Muhammad Iqbal Aditama",
+    });
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+});
